c366e2e82fe7a6ade6f1c59cb6f4f8b3f7d65d2d
PatrykPora <patryk.pora@shitcoins.club>
2024-07-11 17:39:25 +0200

    clientCell

---
+++ src/main/java/pl/pepe/bankapp/controllers/admin/ClientCellController.java
@@ -0,0 +1,23 @@
+package pl.pepe.bankapp.controllers.admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ClientCellController implements Initializable {
+    public Label fname_label;
+    public Label lname_label;
+    public Label peyee_address_label;
+    public Label ror_number_label;
+    public Label saving_acc_label;
+    public Label date_creaated_label;
+    public Button delete_label;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
